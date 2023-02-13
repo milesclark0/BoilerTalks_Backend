3ad88278c93a1d49f274f4c2a0d65ee8cfb33f81
milesclark0 <clark658@purdue.edu>
2023-02-13 02:01:53 -0500

    Added jwt token auth, revised login functionality

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, jsonify, Blueprint, current_app
-from flask_cors import CORS
+from flask import request, jsonify, Blueprint, current_app
+from flask_jwt_extended import create_access_token, jwt_required, create_refresh_token, get_jwt_identity
 from http import HTTPStatus
-from flask_socketio import SocketIO, emit, Namespace
-import jwt, datetime
-from functools import wraps
+#from functools import wraps
 import app.queries.auth.authQueries as queries
+from app.models.User import User
 
 bp = Blueprint('auth', __name__)
 
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -3,21 +3,37 @@ routePrefix = '/auth'
 
 @bp.route(routePrefix + '/login', methods=['POST'])
 def login():
-    #TODO: Implement login
-    user = queries.login(request.json["username"], request.json["password"])
-    return jsonify({"data": user})
-
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return jsonify({'data': None, 'statusCode': HTTPStatus.BAD_REQUEST, 'message': 'Credentials are missing!'})
+    res = queries.login(auth.username, auth.password)
+    if not res.success or not isinstance(res.data, User):
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.UNAUTHORIZED, 'message': res.message})
+    
+    # generate tokens
+    access_token = create_access_token(identity = str(res.data.getId()))
+    refresh_token = create_refresh_token(identity = str(res.data.getId()))
+    
+    return jsonify({'data': {'accessToken': access_token, 'refreshToken': refresh_token}, 'statusCode': HTTPStatus.OK, 'message': res.message})   
 
 @bp.route(routePrefix + '/register', methods=['POST'])
+@jwt_required()
 def register():
     #TODO: Implement register
-    userInfo = queries.register(request.json)
-    return jsonify({"data": userInfo})
+    #userInfo = queries.register(request.json)
+    return jsonify({"data": "hello"})
 
-@bp.route(routePrefix + '/register', methods=['GET'])
+@bp.route(routePrefix + '/registerCourses', methods=['GET'])
 def registerCourses():
     #TODO: Retrieve courses
     courses = queries.getCourses()
     return jsonify({"data": courses})
+
+@bp.route(routePrefix + '/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    return jsonify({'data': {'accessToken': access_token}, 'statusCode': HTTPStatus.OK, 'message': 'Token refreshed'})
 
 
