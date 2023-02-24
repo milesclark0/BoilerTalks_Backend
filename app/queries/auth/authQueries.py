0ddf023682633d4aa44e7b4ce0d67062e43c7a84
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-02-24 17:18:20 -0500

    bug fix for register when email and username are taken

--- app/queries/auth/authQueries.py
+++ app/queries/auth/authQueries.py
@@ -27,6 +27,8 @@ def register(userData):
     except Exception as e:
         return DBreturn(False, "Error occured while saving user", str(e))
     if not res.success:
+        if "Username" or "Email" in res.message:
+            res.data = [res.message]
         return DBreturn(False, "Register Unsuccessful", res.data)
     if not resP.success:
         return DBreturn(False, "Profile Creation Unsuccessful", res.data)
