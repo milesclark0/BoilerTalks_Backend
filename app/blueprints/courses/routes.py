82f5ec4672965ae9fb0bc0934e047adb40fb5407
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-03-18 22:45:50 -0400

    added routes for course management

--- app/blueprints/courses/routes.py
+++ app/blueprints/courses/routes.py
@@ -119,7 +119,32 @@ def addAppealtoCourse(courseId):
     res = DBreturn(False, 'No Course Provided', None)
     if courseId is None or courseId == '':
         return jsonify({'data': res.data, 'statusCode': HTTPStatus.BAD_REQUEST, 'message': res.message})
+    
     res = queries.addAppealforCourse(courseId)
+    if not res.success:
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': res.message})
+    return jsonify({'data': res.data, 'statusCode': HTTPStatus.OK, 'message': res.message})
+
+@bp.route(routePrefix + '/updateAppeal/<courseId>', methods=['POST'])
+@jwt_required()
+def updateAppealtoCourse(courseId):
+    res = DBreturn(False, 'No Course Provided', None)
+    if courseId is None or courseId == '':
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.BAD_REQUEST, 'message': res.message})
+    decision = request.json["decision"]
+    res = queries.updateAppealforCourse(courseId, decision)
+    if not res.success:
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': res.message})
+    return jsonify({'data': res.data, 'statusCode': HTTPStatus.OK, 'message': res.message})
+
+@bp.route(routePrefix + '/banUser/<courseId>', methods=['POST'])
+@jwt_required()
+def banUser(courseId):
+    res = DBreturn(False, 'No Course Provided', None)
+    if courseId is None or courseId == '':
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.BAD_REQUEST, 'message': res.message})
+    user = request.json["user"]
+    res = queries.banUserForCourse(courseId, user)
     if not res.success:
         return jsonify({'data': res.data, 'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': res.message})
     return jsonify({'data': res.data, 'statusCode': HTTPStatus.OK, 'message': res.message})
--- app/queries/courses/courseQueries.py
+++ app/queries/courses/courseQueries.py
@@ -246,3 +246,40 @@ def addAppealforCourse(courseId: str):
         res.message = 'Error occurred while adding appeal to course'
         res.data = str(e)
     return res
+
+def updateAppealforCourse(courseId: str, descision: str):
+    res = DBreturn()
+    try:
+        course = CourseManagement.collection.find_one({"courseId": courseId})
+        if course is None:
+            res.message = 'get course error: no course found'
+            return res
+        # if decision is unban, remove user from ban array
+        # if decision is deny, update reason in ban array
+        # remove the appeal
+        res.data = parse_json(course)
+        res.success = True
+        res.message = 'Successfully updated ban to course'
+    except Exception as e:
+        res.success = False
+        res.message = 'Error occurred while updating ban to course'
+        res.data = str(e)
+    return res
+
+def banUserForCourse(courseId: str, username: str):
+    res = DBreturn()
+    try:
+        course = CourseManagement.collection.find_one({"courseId": courseId})
+        if course is None:
+            res.message = 'get course error: no course found'
+            return res
+        # find user from username
+        # add user to ban list
+        res.data = parse_json(course)
+        res.success = True
+        res.message = 'Successfully added user to ban list'
+    except Exception as e:
+        res.success = False
+        res.message = 'Error occurred while adding user to ban list'
+        res.data = str(e)
+    return res
