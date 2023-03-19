da32963d1b69043cdc03fe86f16005c839c9ba1d
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-03-18 23:00:39 -0400

    moved coursemanagement routes

---
+++ app/blueprints/courseManagement/__init__.py
@@ -0,0 +1,11 @@
+from flask import request, jsonify, Blueprint, current_app
+from flask_jwt_extended import jwt_required
+from http import HTTPStatus
+#from functools import wraps
+import app.queries.courseManagement.courseManagementQueries as queries
+from app.models.Course import Course
+from app.models.Database import db, DBreturn, parse_json, ObjectId
+
+bp = Blueprint('courseManagement', __name__)
+
+from app.blueprints.courseManagement import routes
