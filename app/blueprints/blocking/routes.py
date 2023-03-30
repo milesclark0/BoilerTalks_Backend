2028b5bb78ee7838b18437067a97c712434bfa64
oneKZhang <zhan3461@purdue.edu>
2023-03-30 00:36:02 -0400

    added unblocking, fixed ambiguities

--- app/blueprints/blocking/routes.py
+++ app/blueprints/blocking/routes.py
@@ -11,8 +11,25 @@ def blockUser():
     except KeyError as e:
         return jsonify({'data': str(e), 'statusCode': HTTPStatus.BAD_REQUEST, 'message': res.message})
     
-    print("Attempting to block " + toBlock + " on user " + username)
     res = queries.blockUser(toBlock, username)
+
+    if not res.success:
+        return jsonify({'data': res.data, 'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': res.message})
+    return jsonify({'data': res.data, 'statusCode': HTTPStatus.OK, 'message': res.message})
+
+@bp.route(routePrefix + '/unblockUser', methods=['POST'])
+@jwt_required()
+def unblockUser():
+    res = DBreturn(True, 'Unblock not performed', None)
+    try:
+        toUnblock = request.json['toUnblock']
+        username = request.json['username']
+        print("Backend trying to unblock " + toUnblock)
+    except KeyError as e:
+        return jsonify({'data': str(e), 'statusCode': HTTPStatus.BAD_REQUEST, 'message': res.message})
+    
+    res = queries.unblockUser(toUnblock, username)
+    print("successful unblock?")
 
     if not res.success:
         return jsonify({'data': res.data, 'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': res.message})
--- app/queries/blocking/blockingQueries.py
+++ app/queries/blocking/blockingQueries.py
@@ -8,13 +8,13 @@ def blockUser(toBlock: str, username: str):
             res.message = 'error blocking user: blocker user not found'
             return res
         
-        toBlock = User.collection.find_one({"username": toBlock})
-        if toBlock is None:
+        blockTarget = User.collection.find_one({"username": toBlock})
+        if blockTarget is None:
             res.message = 'error blocking user: user to block not found'
             return res
         
         userDict = User.fromDict(user)
-        blockDict = User.fromDict(toBlock)
+        blockDict = User.fromDict(blockTarget)
 
         if toBlock in userDict.getBlockedUsers():
             res.message = 'error blocking user: user already blocked'
@@ -35,6 +35,47 @@ def blockUser(toBlock: str, username: str):
     except Exception as e:
         res.success = False
         res.message = 'Error occurred while blocking user'
+        res.data = str(e)
+        print(res.data)
+    return res  
+
+def unblockUser(toUnblock: str, username: str):
+    res = DBreturn()
+    try:
+        user = User.collection.find_one({"username": username})
+        if user is None:
+            res.message = 'error blocking user: unblocker user not found'
+            return res
+        
+        unblockTarget = User.collection.find_one({"username": toUnblock})
+        if unblockTarget is None:
+            res.message = 'error blocking user: user to unblock not found'
+            return res
+        
+        userDict = User.fromDict(user)
+        unblockDict = User.fromDict(unblockTarget)
+
+        if toUnblock not in userDict.getBlockedUsers():
+            res.message = 'error blocking user: user already unblocked'
+            return res
+        
+        print(userDict.getUsername())
+
+        blockedUserList = userDict.getBlockedUsers()
+        blockedUserList.remove(unblockDict.getUsername())
+
+        print(blockedUserList)
+
+        userDict.setBlockedUsers(blockedUserList)
+        userSaveResult = userDict.update()
+        if not userSaveResult.success:
+            return userSaveResult
+        res.success = True
+        res.message = 'Successfully unblocked user'
+
+    except Exception as e:
+        res.success = False
+        res.message = 'Error occurred while unblocking user'
         res.data = str(e)
         print(res.data)
     return res  
